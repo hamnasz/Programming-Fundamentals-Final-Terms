a7c2535152dc8bf9174971f6e1c84e8c01080ee5
Humna Imran <168326829+hamnasz@users.noreply.github.com>
2024-09-21 16:30:24 +0500

    By Hamna Mirza

---
+++ Labs/Lab5/positive negative zero.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int num;
+	cout<<"Enter num: ";
+	cin>>num;
+	if(num==0)
+	cout<<"Number is zero";
+	else if(num>0)
+	cout<<"Number is positive";
+	else cout<<"number is negative";
+}
