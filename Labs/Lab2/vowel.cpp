2070c60ab431c79bfc6d47c42c6aec39ba09b8a7
Humna Imran <168326829+hamnasz@users.noreply.github.com>
2024-09-21 16:25:42 +0500

    By Hamna Mirza

---
+++ Labs/Lab2/vowel.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+	char ch;
+	cout<<"Enter alphabet: ";
+	cin>>ch;
+	if(ch=='a'||ch=='A'||ch=='e'||ch=='E'||ch=='i'||ch=='I'||ch=='o'||ch=='O'||ch=='u'||ch=='U')
+	cout<<"Vowel";
+	else cout<<"Consonant";
+}
