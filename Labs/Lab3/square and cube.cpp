e6244bb341484188a7a948f1cfc564bb94a0a58f
Humna Imran <168326829+hamnasz@users.noreply.github.com>
2024-09-21 16:27:14 +0500

    By Hamna Mirza

---
+++ Labs/Lab3/square and cube.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int num1,num2,num3;
+	cout<<"Enter Number 1:\t";
+	cin>>num1;
+	cout<<"Enter Number 2:\t";
+	cin>>num2;
+	cout<<"Enter Number 3:\t";
+	cin>>num3;
+	cout<<"\nNumber\tSquare\tCube"<<endl;
+	cout<<num1<<"\t"<<num1*num1<<"\t"<<num1*num1*num1<<endl;
+	cout<<num2<<"\t"<<num2*num2<<"\t"<<num2*num2*num2<<endl;
+	cout<<num3<<"\t"<<num3*num3<<"\t"<<num3*num3*num3<<endl;
+}
