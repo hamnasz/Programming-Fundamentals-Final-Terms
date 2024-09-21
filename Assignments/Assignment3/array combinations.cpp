a0bc0996dd996460ca4c66185545506f663f0b4c
Humna Imran <168326829+hamnasz@users.noreply.github.com>
2024-09-21 16:47:49 +0500

    By Hamna Mirza

---
+++ Assignments/Assignment3/array combinations.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 3;
+    int arr[n];
+
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int c;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            for (int k = 0; k < n; k++)
+            {
+                c++;
+                cout << arr[i] << " " << arr[j] << " " << arr[k] << endl;
+            }
+        }
+    }
+    cout << c;
+    return 0;
+}
